a8c9e75f2fbca9dca2e1421fc1b636ee245057c4
IraSheremet <irasheremet2002@gmail.com>
2023-01-18 22:00:17 +0300

    function getUser

--- tms-ts/src/services/profile.service.tsx
+++ tms-ts/src/services/profile.service.tsx
@@ -5,4 +5,8 @@ export default class ProfileService {
                       user: { username: string; password: string; first_name?: string, last_name?: string, email?: string }) {
         return axiosTMS.put("api/v1/users/" + id + "/", user)
     }
+
+    static getUser(id: number) {
+        return axiosTMS.get("api/v1/users/" + id)
+    }
 }
